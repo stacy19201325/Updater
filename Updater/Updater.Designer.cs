9f486ffc1cb6ab04f0475db9a4dcae52563e8813
Trakaa <trakaa@gmail.com>
2017-07-29 19:06:59 -0400

    Self sign disabled Font changed to Castellar

--- Updater/Updater.Designer.cs
+++ Updater/Updater.Designer.cs
@@ -41,7 +41,7 @@
             this.btnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(192)))), ((int)(((byte)(63)))));
             this.btnMain.FlatAppearance.BorderSize = 0;
             this.btnMain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMain.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMain.Font = new System.Drawing.Font("Castellar", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMain.Location = new System.Drawing.Point(551, 169);
             this.btnMain.Name = "btnMain";
             this.btnMain.Size = new System.Drawing.Size(150, 50);
@@ -63,11 +63,11 @@
             // lblSettings
             // 
             this.lblSettings.AutoSize = true;
-            this.lblSettings.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSettings.Font = new System.Drawing.Font("Castellar", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(192)))), ((int)(((byte)(63)))));
-            this.lblSettings.Location = new System.Drawing.Point(596, 12);
+            this.lblSettings.Location = new System.Drawing.Point(586, 12);
             this.lblSettings.Name = "lblSettings";
-            this.lblSettings.Size = new System.Drawing.Size(63, 19);
+            this.lblSettings.Size = new System.Drawing.Size(70, 13);
             this.lblSettings.TabIndex = 0;
             this.lblSettings.Text = "Settings";
             this.lblSettings.Click += new System.EventHandler(this.lblSettings_Click);
@@ -75,11 +75,11 @@
             // lblExit
             // 
             this.lblExit.AutoSize = true;
-            this.lblExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExit.Font = new System.Drawing.Font("Castellar", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(192)))), ((int)(((byte)(63)))));
-            this.lblExit.Location = new System.Drawing.Point(665, 12);
+            this.lblExit.Location = new System.Drawing.Point(662, 12);
             this.lblExit.Name = "lblExit";
-            this.lblExit.Size = new System.Drawing.Size(36, 19);
+            this.lblExit.Size = new System.Drawing.Size(39, 13);
             this.lblExit.TabIndex = 1;
             this.lblExit.Text = "Exit";
             this.lblExit.Click += new System.EventHandler(this.lblExit_Click);
@@ -89,7 +89,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(716, 233);
+            this.ClientSize = new System.Drawing.Size(721, 233);
             this.ControlBox = false;
             this.Controls.Add(this.lblExit);
             this.Controls.Add(this.lblSettings);
